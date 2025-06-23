62d2350b688eb1dafc3f22953b56888d7a4ce30d
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2025-06-23 16:09:18 -0300

    Updated Github link at AboutPanel

--- src/Components/AboutPanel.jsx
+++ src/Components/AboutPanel.jsx
@@ -6,7 +6,7 @@ function AboutPanel() {
         <div className={styles.container}>
             <h4>TinyCPU Simulator</h4>
             <p>A free web-based simulation environment to execute Assembly instructions of teaching-purpose TinyCPU machine.</p>
-            <p style={{textAlign:"right"}}>Available at <a target="_blank" rel="noreferrer" href="https://github.com/fmsampaio/tinycpu-sim-frontend-web">Github</a></p>
+            <p style={{textAlign:"right"}}>Available at <a target="_blank" rel="noreferrer" href="https://github.com/fmsampaio/tinycpu-sim-web">Github</a></p>
         </div>
     )
 }
